d88165107945d66f67494f7e7dc6b71ae9263149
kouadiopatrice <kouadiopatrice428@gmail.com>
2021-02-17 11:57:21 +0000

    commit de soumit

---
+++ partie8/souscription.js
@@ -0,0 +1,12 @@
+
+const { gql } = require("apollo-server");
+
+const subscription = gql`
+  type Subscription {
+    bookAdded: Book!
+  }
+`;
+
+module.exports = {
+  subscription,
+};
---
+++ partie8/type/userType.js
@@ -0,0 +1,16 @@
+const { gql } = require("apollo-server");
+
+const userType = gql`
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+  type Token {
+    value: String!
+  }
+`;
+
+module.exports = {
+  userType,
+};
